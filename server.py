06a2cca07f7296825f046107ff80776a42ea074a
Curtis <477623879@qq.com>
2017-03-12 23:50:59 +0800

    实现用户信息搜索接口 & 详情接口

--- server.py
+++ server.py
@@ -11,9 +11,9 @@
 #         return iter([data])
 
 
-from flask import Flask
+from flask import Flask, jsonify
 import urllib2
-
+import json
 # 解决unicode错误
 import sys
 reload(sys)
@@ -21,22 +21,39 @@ sys.setdefaultencoding('utf-8')
 
 
 # ------- methods -------
-def getUserListBySummonerName(summoner):
-    requestUrl = 'http://api.lolbox.duowan.com/api/v2/player/search/?player_name_list=%s&callback=jQuery111200161216930093' \
+def get_user_list_by_summoner_name(summoner):
+    request_url = 'http://api.lolbox.duowan.com/api/v2/player/search/?player_name_list=%s&callback=jQuery111200161216930093' \
           '95033_1470488155157&_=1470488155158' % summoner
-    res = urllib2.urlopen(requestUrl)
-    html = res.read()[44:-1]
-    return html
+    res = urllib2.urlopen(request_url)
+    return res_handler(res);
+
+
+def get_detail_by_user_id(id):
+    request_url = "http://api.lolbox.duowan.com/api/v3/player/dx1/%s/?callback=jQuery111101163928349854364_1489317803513&_=1489317803514" \
+    % id
+    res = urllib2.urlopen(request_url)
+    return res_handler(res);
+
+def res_handler(res):
+    raw = res.read()
+    first_char_index = raw.index('(')+1
+    return raw[first_char_index:-1]
+
 
 # ------- server -------
 application = Flask(__name__)
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Hello Summoner!</h1>"
 
 @application.route("/search/<summoner>")
 def search(summoner):
-    return getUserListBySummonerName(summoner)
+    return get_user_list_by_summoner_name(summoner)
+
+@application.route("/user/detail/<id>")
+def userDetail(id):
+    return get_detail_by_user_id(id)
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
